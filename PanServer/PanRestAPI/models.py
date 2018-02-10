0adfb173b7e930784acd6757cdce86e37a54ef5f
aknobloch <aaronknobloch92@gmail.com>
2018-02-10 12:29:27 -0500

    add database models

--- PanServer/PanRestAPI/models.py
+++ PanServer/PanRestAPI/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# Defines a webpage
+class WebPage(models.Model):
+	URL = models.TextField()
+	Content = models.TextField()
+
+# Defines a PageEntity, which maps
+# a webpage to it's entities. 
+class PageEntity(models.Model):
+	WebPageID = models.ForeignKey(WebPage, on_delete=models.CASCADE)
+	EntityID = models.ForeignKey(Entity, on_delete=models.CASCADE)
+
+# Defines an entity, or a subject, and it's 
+# related Wiki link.
+class Entity(models.Model):
+	EntityName = models.CharField(max_length = 255)
+	WikiLink = models.TextField()
